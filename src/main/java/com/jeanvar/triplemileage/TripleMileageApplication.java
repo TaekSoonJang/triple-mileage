9b64bb8f5109280fb6786895f9063535030a139d
Taeksoon Jang <taeksoon.jang@gmail.com>
2019-04-07 09:46:59 +0900

    Initial commit with dependencies added.

---
+++ src/main/java/com/jeanvar/triplemileage/TripleMileageApplication.java
@@ -0,0 +1,13 @@
+package com.jeanvar.triplemileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleMileageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleMileageApplication.class, args);
+	}
+
+}
