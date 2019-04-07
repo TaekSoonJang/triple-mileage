983158421ce3483ad3573847b40d6a7e9bbc96ad
Taeksoon Jang <taeksoon.jang@gmail.com>
2019-04-07 11:59:22 +0900

    Set associations in Review with User and Place.

--- src/main/java/com/jeanvar/triplemileage/domain/Review.java
+++ src/main/java/com/jeanvar/triplemileage/domain/Review.java
@@ -3,15 +3,21 @@ package com.jeanvar.triplemileage.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "REVIEW")
 @Getter
 @Setter
 public class Review extends TripleEntity {
+    @JoinColumn
+    @ManyToOne
+    private User user;
+
+    @JoinColumn
+    @ManyToOne
+    private Place place;
+
     @Column
     private String content;
 }
--- src/test/java/com/jeanvar/triplemileage/repository/ReviewRepositoryTest.java
+++ src/test/java/com/jeanvar/triplemileage/repository/ReviewRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.jeanvar.triplemileage.repository;
 
+import com.jeanvar.triplemileage.domain.Place;
 import com.jeanvar.triplemileage.domain.Review;
+import com.jeanvar.triplemileage.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -24,5 +26,30 @@ class ReviewRepositoryTest {
         Review saved = reviewRepository.save(review);
 
         assertThat(reviewRepository.findById(saved.getId())).get().isEqualTo(saved);
+    }
+
+    @Test
+    void join() {
+        User user = new User();
+        user.setName("taeksoon");
+        user = entityManager.persist(user);
+
+        Place place = new Place();
+        place.setName("new place");
+        place = entityManager.persist(place);
+
+        Review review = new Review();
+        review.setUser(user);
+        review.setPlace(place);
+        review.setContent("Liked!");
+
+        Review saved = reviewRepository.save(review);
+        entityManager.flush();
+        entityManager.clear();
+
+        Review found = reviewRepository.findById(saved.getId()).get();
+
+        assertThat(found.getUser().getId()).isEqualTo(user.getId());
+        assertThat(found.getPlace().getId()).isEqualTo(place.getId());
     }
 }
